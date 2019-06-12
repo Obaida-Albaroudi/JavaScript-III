0b7bf4b176945bdebdeb5a1b54704123e931f0a4
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-06-12 14:00:47 -0700

    Finished the project, starting to work on the stretch goals now.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -8,101 +8,111 @@
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
   
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
+function GameObject(details){
+  this.createdAt = details.createdAt;
+  this.name = details.name;
+  this.dimensions = details.dimensions;
+};
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+GameObject.prototype.destroy = function(){
+  return this.name + " was removed from the game."
+};
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- 
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+
+//  === CharacterStats ===
+
+function CharacterStats(playerstats){
+  GameObject.call(this, playerstats);
+  this.healthPoints = playerstats.healthPoints;
+};
+CharacterStats.prototype= Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return this.name + " took damage."
+};
+
+
+//  === Humanoid (Having an appearance or character resembling that of a human.) ===
+
+function Humanoid(info){
+  GameObject.call(this, info);
+  CharacterStats.call(this, info);
+  this.team = info.team;
+  this.weapons = info.weapons;
+  this.language = info.language;
+};
+
+Humanoid.prototype= Object.create(GameObject.prototype);
+Humanoid.prototype= Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet= function(){
+  return this.name + " offers a greeting in " + this.language
+};
+
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. It will be referencing the parameter given in the global environment (console). As in the this will bereferencing the cosole as its object.
+* 2. The implicit binding here would be using the this statement on a function directly associated with it by saying function.this or using the this inside of a function.
+* 3. This is specific to a constructor function dealing with Objects. It allows us to create as many objects as we would like without needing to rewrite everything. We will need the term new to trigger.
+* 4. This will prioritize and give precedence to the functions with .call and .apply.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,65 @@
 
 // code example for Window Binding
 
+function introduction(parameter){
+    console.log(this);
+    return parameter;
+};
+
+introduction("Obaida");
+
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const properintroduction={
+    greeting: "Hello, ",
+    asking: "how are you doing on this fine day? ",
+    fullconvo: function(name){
+      console.log(this.greeting + this.asking + name);
+    }
+};
+
+properintroduction.fullconvo("Adetunji");
 
 // Principle 3
 
 // code example for New Binding
 
+function web21(name){
+    this.thanks= "Thank you for being a dope teacher ";
+    this.name = name;
+    this.join= function(){
+      console.log(this.thanks + this.name);
+      console.log(this)
+    };
+}
+  
+const web21students = new web21("Dan");
+const random_class = new web21("Lous Williams");
+
+web21students.join();
+random_class.join();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+function web21(name){
+    this.thanks= "Thank you for being a dope teacher ";
+    this.name = name;
+    this.join= function(){
+      console.log(this.thanks + this.name);
+      console.log(this)
+    };
+}
+
+const web21students = new web21("Dan");
+const random_class = new web21("Lous Williams");
+
+web21students.join();
+random_class.join();
+
+web21students.join.apply(random_class);
+random_class.join.call(web21students);
